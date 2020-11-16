014071789438c3bbceccdb79940082ea5a6d44cb
Nigel Kitcher <nigel@nkitcher.co.uk>
2020-11-16 09:57:55 +0000

    feat: add consuming app as an example

---
+++ Otb.JobSequencer/Otb.JobSequencer.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Otb.JobSequencer.Service;
+
+namespace Otb.JobSequencer.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jobRequestParser = new JobRequestParser();
+            var sequencerService = new SequencerService();
+
+            var input = "A => B" + Environment.NewLine + "B =>";
+
+            var jobs = jobRequestParser.GetJobs(input);
+
+            var sequencedJobs = sequencerService.GetTopologicalOrdering(jobs);
+
+            Console.WriteLine("Input Jobs:");
+            Console.WriteLine(input);
+
+            Console.WriteLine("Sequenced Jobs:");
+            foreach (var job in sequencedJobs)
+            {
+                Console.Write(job.Name);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Completed.");
+        }
+    }
+}
